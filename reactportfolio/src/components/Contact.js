70585459ca60b2f59d00783c6fcfe1d10d7d2196
Jean Tubay <jean.tubay.92@gmail.com>
2020-01-17 11:12:15 -0500

    added styling and content to projects section and contact me section

--- reactportfolio/src/components/Contact.js
+++ reactportfolio/src/components/Contact.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import { Grid, Cell } from 'react-mdl';
 
 class Contact extends Component {
   render() {
     return (
-      <div>
-        <h1>Contact Page</h1>
+      <div className="contact-body">
+        <Grid className="contact-grid">
+            <Cell col={6}>
+                <h2>Jean Tubay</h2>
+                <img 
+                    src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                    alt="avatar"
+                    style={{height: "250px"}}
+                />
+                <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam minima vitae quia ex voluptas sit fugit modi quae, nisi sed, exercitationem excepturi reprehenderit assumenda recusandae pariatur harum molestias. Tenetur, soluta!</p>
+            </Cell>
+            <Cell col={6}>
+              <h2>Contact Me</h2>
+              <hr/>
+            </Cell>
+        </Grid>
+        
       </div>
     );
   }
--- reactportfolio/src/components/Projects.js
+++ reactportfolio/src/components/Projects.js
@@ -1,13 +1,369 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton,
+  CardText
+} from "react-mdl";
 
 class Projects extends Component {
-    render() {
-        return(
-            <div>
-                <h1>Projects Page</h1>
-            </div>
-        )
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div className="projects-grid" style={{paddingTop: '50px'}}>
+            {/* Project 1 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover"
+              }}
+            >
+              React Project #1
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share"/>
+            </CardMenu>
+          </Card>
+            {/* Project 2 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover"
+              }}
+            >
+              React Project #1
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share"/>
+            </CardMenu>
+          </Card>
+            {/* Project 3 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover"
+              }}
+            >
+              React Project #1
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share"/>
+            </CardMenu>
+          </Card>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div className="projects-grid" style={{paddingTop: '50px'}}>
+        {/* Project 1 */}
+      <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+        <CardTitle
+          style={{
+            color: "#fff",
+            height: "176px",
+            background:
+              "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover"
+          }}
+        >
+          React Project #1
+        </CardTitle>
+        <CardText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          sagittis pellentesque lacus eleifend lacinia...
+        </CardText>
+        <CardActions border>
+          <Button colored>GitHub</Button>
+          <Button colored>CodePen</Button>
+          <Button colored>Live Demo</Button>
+        </CardActions>
+        <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share"/>
+        </CardMenu>
+      </Card>
+        {/* Project 2 */}
+      <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+        <CardTitle
+          style={{
+            color: "#fff",
+            height: "176px",
+            background:
+              "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover"
+          }}
+        >
+          React Project #1
+        </CardTitle>
+        <CardText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          sagittis pellentesque lacus eleifend lacinia...
+        </CardText>
+        <CardActions border>
+          <Button colored>GitHub</Button>
+          <Button colored>CodePen</Button>
+          <Button colored>Live Demo</Button>
+        </CardActions>
+        <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share"/>
+        </CardMenu>
+      </Card>
+        {/* Project 3 */}
+      <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+        <CardTitle
+          style={{
+            color: "#fff",
+            height: "176px",
+            background:
+              "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover"
+          }}
+        >
+          React Project #1
+        </CardTitle>
+        <CardText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          sagittis pellentesque lacus eleifend lacinia...
+        </CardText>
+        <CardActions border>
+          <Button colored>GitHub</Button>
+          <Button colored>CodePen</Button>
+          <Button colored>Live Demo</Button>
+        </CardActions>
+        <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share"/>
+        </CardMenu>
+      </Card>
+    </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div className="projects-grid" style={{paddingTop: '50px'}}>
+            {/* Project 1 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover"
+              }}
+            >
+              React Project #1
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share"/>
+            </CardMenu>
+          </Card>
+            {/* Project 2 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover"
+              }}
+            >
+              React Project #1
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share"/>
+            </CardMenu>
+          </Card>
+            {/* Project 3 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover"
+              }}
+            >
+              React Project #1
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share"/>
+            </CardMenu>
+          </Card>
+        </div>
+      );
+    } else if (this.state.activeTab === 3) {
+      return (
+        <div className="projects-grid" style={{paddingTop: '50px'}}>
+            {/* Project 1 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover"
+              }}
+            >
+              React Project #1
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share"/>
+            </CardMenu>
+          </Card>
+            {/* Project 2 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover"
+              }}
+            >
+              React Project #1
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share"/>
+            </CardMenu>
+          </Card>
+            {/* Project 3 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover"
+              }}
+            >
+              React Project #1
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share"/>
+            </CardMenu>
+          </Card>
+        </div>
+      );
     }
+  }
+  render() {
+    return (
+      <div>
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>Angular</Tab>
+          <Tab>VueJs</Tab>
+          <Tab>MongoDB</Tab>
+        </Tabs>
+       
+          <Grid >
+            <Cell col={12}>
+              <div className="content">{this.toggleCategories()} </div>
+            </Cell>
+          </Grid>
+       
+      </div>
+    );
+  }
 }
 
 export default Projects;
